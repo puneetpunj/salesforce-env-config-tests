0ccaa85e80a4f29c5e07aeb12e4222c784f0b105
Puneet Punj <puneetpunj@puneets-mbp.home>
2020-04-07 21:46:24 +1000

    added extra check to ensure credential are updated

--- build-env-tests.js
+++ build-env-tests.js
@@ -1,7 +1,8 @@
-const { buildVLOVTestsforAllObjects, buildValidationRulesTestsforAllObjects } = require('./lib/utiltities');
+const { buildVLOVTestsforAllObjects, buildValidationRulesTestsforAllObjects, sampleCredentialsPresent } = require('./lib/utiltities');
 
 (async () => {
     const objectList = ['contact', 'account', 'lead'];
+    if (sampleCredentialsPresent()) { console.error('Set your credentials in the env specific config file'); return }
 
     const r = await buildValidationRulesTestsforAllObjects(objectList)
     console.log(r)
