e7aa7e10c6ef8f1bdb79c04b844e0a2a41f55016
Puneet Punj <puneetpunj@puneets-mbp.home>
2020-04-08 12:59:31 +1000

    fetch object list from extra file using fs-extra

--- build-env-tests.js
+++ build-env-tests.js
@@ -1,13 +1,10 @@
-const { buildVLOVTestsforAllObjects, buildValidationRulesTestsforAllObjects, sampleCredentialsPresent } = require('./lib/utiltities');
+const { buildVLOVTestsforAllObjects, getObjectListFromInputFile, buildValidationRulesTestsforAllObjects, sampleCredentialsPresent } = require('./lib/utiltities');
 
 (async () => {
-    const objectList = ['contact', 'account', 'lead'];
-    if (sampleCredentialsPresent()) { console.error('Set your credentials in the env specific config file'); return }
-
+    const objectList = getObjectListFromInputFile();
+    if (sampleCredentialsPresent()) { console.error('\x1b[31m%s\x1b[0m', 'Error: \n Set your credentials in the env specific config file. \n Set in config/config.<env>.json'); return }
     const r = await buildValidationRulesTestsforAllObjects(objectList)
     console.log(r)
-
     const a = await buildVLOVTestsforAllObjects(objectList)
     console.log(a)
-
 })()
