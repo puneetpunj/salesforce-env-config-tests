ea110b844e4f7555246ba87e93c5163059eeb586
Puneet Punj <puneetpunj88@gmail.com>
2020-04-11 14:13:26 +1000

    updated for new config file checks and add logging

--- comparison.js
+++ comparison.js
@@ -1,22 +1,24 @@
-const fs = require('fs-extra');
-const path = require('path')
-const { ERROR, INFO, WARNING, SUCCESS, LOG } = require('./lib/logging')
-const { sampleCredentialsPresent, sendSalesforceQuery } = require('./lib/utiltities');
-const inscopeData = fs.readJSONSync('./inscope-object-list.json');
-const { BuildTests } = require('./build-env-tests');
+const { readJSONSync } = require('fs-extra');
+const { ERROR, INFO, WARNING, SUCCESS, LOG, TABLE } = require('./lib/logging')
+const { sampleCredentialsPresent, sendSalesforceQuery, readConfigFile } = require('./lib/utiltities');
+const inscopeData = readJSONSync('./inscope-object-list.json');
+const { AutoGenerateTests } = require('./build-env-tests');
 const { DefineTests } = require('./test-scripts/define-tests')
 const { ExecuteTests } = require('./test-scripts/execute-tests')
 
-const checkConfigFileExists = (baseOrg, destinationOrg) => {
-    try {
-        fs.readJSONSync(path.resolve(__dirname, `./config/config.${baseOrg}.json`));
-        destinationOrg.forEach(file => fs.readJSONSync(path.resolve(__dirname, `./config/config.${file}.json`)))
-        return true
-    }
-    catch (e) {
-        ERROR(e)
-        return false
-    }
+const checkSalesforceCredKeysAvailable = creds => typeof (creds) == 'object' ? true : false
+
+const checkConfigExists = (baseOrg, destinationOrg) => {
+
+    const configFileDetails = readConfigFile();
+    const baseOrgCreds = configFileDetails.loginDetails[baseOrg]
+    const baseOrgCredsCheck = checkSalesforceCredKeysAvailable(baseOrgCreds)
+    const destOrgCredsCheck = destinationOrg.map(org => {
+        const destOrgCreds = configFileDetails.loginDetails[org]
+        const destOrgCredsCheck = checkSalesforceCredKeysAvailable(destOrgCreds)
+        return destOrgCredsCheck
+    })
+    return baseOrgCredsCheck && destOrgCredsCheck.reduce((acc, i) => { acc = i && acc; return acc }, true)
 }
 
 const checkSampleCredentialsForAllEnv = (baseOrg, destinationOrgs) => {
@@ -34,10 +36,10 @@ const getOnlyValidObjects = (baseArray, secondaryArray, orgName) => {
     return validObjects;
 }
 
-const getvalidObjectsForAnOrg = async (Org) => {
+const getvalidObjectsForAnOrg = async Org => {
     const inputObjectList = inscopeData[`${Org}.objectList`]
 
-    INFO(`Input objects list for ${Org} is ${inputObjectList}`)
+    INFO(`Input objects list for ${Org} org is [${inputObjectList}]`)
 
     const objectListWithQuotes = "'" + inputObjectList.join("','") + "'";
     const query = `Select QualifiedApiName from EntityDefinition where QualifiedApiName IN (${objectListWithQuotes}) order by QualifiedApiName`
@@ -45,20 +47,21 @@ const getvalidObjectsForAnOrg = async (Org) => {
     if (typeof (OrgObjectsInSalesforce) == 'string' && OrgObjectsInSalesforce.includes('INVALID_LOGIN')) return OrgObjectsInSalesforce
 
     if (OrgObjectsInSalesforce.totalSize == inputObjectList.length) return inputObjectList
+
     const SalesforceObjectArray = OrgObjectsInSalesforce.records.map(i => i.QualifiedApiName.toLowerCase())
     const validObjects = getOnlyValidObjects(inputObjectList, SalesforceObjectArray, Org)
     return validObjects
 }
 
-const getDestinationValidObjects = async (OrgArray, baseObjectsArray) => {
+const getDestinationOrgsValidObjects = async (OrgArray, baseObjectsArray) => {
     let validDestinationObjects = {}
     for (let i = 0; i < OrgArray.length; i++) {
         const validSalesforceObjects = await getvalidObjectsForAnOrg(OrgArray[i])
         if (validSalesforceObjects.includes('INVALID_LOGIN')) return `Org - ${OrgArray[i]} --> ${validSalesforceObjects}`
 
-        INFO(`Valid Object List from Salesforce for ORG -> ${OrgArray[i]} is -> ${validSalesforceObjects}`)
+        INFO(`Valid Object List from Salesforce for ORG -> [${OrgArray[i]}] is -> ${validSalesforceObjects}`)
 
-        INFO(`Filter further as comparison to Base Org Objects - ${baseObjectsArray}`)
+        INFO(`Filter further as comparison to Base Org Objects - [${baseObjectsArray}]`)
 
         // further filter down based on baseOrg's valid Objects
         validDestinationObjects[OrgArray[i]] = getOnlyValidObjects(baseObjectsArray, validSalesforceObjects, OrgArray[i])
@@ -99,9 +102,9 @@ const checkErrors = () => {
 
     if (!checkObjectsForAllOrgsExists(baseOrg, destinationOrgs)) { ERROR('Looks like "objectList" array does not exist for all base/destination orgs'); return true }
 
-    if (!checkConfigFileExists(baseOrg, destinationOrgs)) { ERROR('Config files for all specified base and destination orgs are not present. Go to config dir to add config files'); return true }
+    if (!checkConfigExists(baseOrg, destinationOrgs)) { ERROR('Config files for all specified base and destination orgs are not present. Go to config dir to add config files'); return true }
 
-    if (checkSampleCredentialsForAllEnv(baseOrg, destinationOrgs)) { ERROR('Looks like you have not setup your credentials in the org specific config file. \n \n Add/update config/config.<env>.json with actual credentials'); return true }
+    if (checkSampleCredentialsForAllEnv(baseOrg, destinationOrgs)) { ERROR('Looks like you have not setup your credentials in the org specific config file. \n \n Add/update config.json with actual credentials'); return true }
 
     return false
 
@@ -121,19 +124,19 @@ const printAsterics = () => LOG('*'.repeat(80));
     const validBaseOrgObjects = await getvalidObjectsForAnOrg(baseOrg)
     if (validBaseOrgObjects.includes('INVALID_LOGIN')) return ERROR(`Your base Org (${baseOrg}) credentials are incorrect`);
     if (validBaseOrgObjects.length == 0) { ERROR('No valid object specified in input file for Base Org'); return }
-    SUCCESS(`Valid Object List for Base Org - ${validBaseOrgObjects}`)
+    SUCCESS(`Valid Object List for Base Org - [${validBaseOrgObjects}]`)
 
     // Fetch Valid Objects for Destination Orgs
     printAsterics()
     LOG('Starting to Get Valid Objects for Destination Orgs')
-    const validDestinationOrgObjects = await getDestinationValidObjects(destinationOrgs, validBaseOrgObjects)
+    const validDestinationOrgObjects = await getDestinationOrgsValidObjects(destinationOrgs, validBaseOrgObjects)
     if (typeof (validDestinationOrgObjects) == 'string' && validDestinationOrgObjects.includes('INVALID_LOGIN')) return ERROR(validDestinationOrgObjects);
-    SUCCESS(`Valid Object List for all Destination Orgs is - ${JSON.stringify(validDestinationOrgObjects)}`)
+    SUCCESS(`Valid Object List for all Destination Orgs is - ${JSON.stringify(validDestinationOrgObjects)} `)
 
     // Build Tests referencing Base Org
     printAsterics()
     LOG('Start auto generation of test files using base org')
-    const buildTestsResponse = await BuildTests(baseOrg, validBaseOrgObjects);
+    const buildTestsResponse = await AutoGenerateTests(baseOrg, validBaseOrgObjects);
     SUCCESS(buildTestsResponse)
 
     // Define Tests for each Destination
